8683df49744a69be8fa740aca8c0d928cfccdb05
Fernando Souza <fernando.de.souza@gmail.com>
2017-01-17 20:53:56 -0200

    inicial junit

---
+++ src/main/java/br/com/caelum/argentum/modelo/Negociacao.java
@@ -0,0 +1,36 @@
+package br.com.caelum.argentum.modelo;
+
+import java.util.Calendar;
+
+public final class Negociacao {
+	private final double preco;
+	private final int quantidade;
+	private final Calendar data;
+
+	public Negociacao(double preco, int quantidade, Calendar data) {
+		
+		if(data == null){
+			throw new IllegalArgumentException("Data não pode ser nula");
+		}
+
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.data = data;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public Calendar getData() {
+		return (Calendar) data.clone();
+	}
+
+	public double getVolume(){
+		return preco * quantidade;
+	}
+}
